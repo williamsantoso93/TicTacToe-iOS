3d0e8973f4ca31412bf490f29eddf5e434bffaef
William Santoso <williamsantoso93@yahoo.com>
2021-02-14 19:48:52 +0700

    initial2

---
+++ TicTacToe/Model/Model.swift
@@ -0,0 +1,21 @@
+//
+//  Model.swift
+//  TicTacToe
+//
+//  Created by William Santoso on 12/02/21.
+//
+
+import Foundation
+
+enum Status: String {
+    case xWin
+    case oWin
+    case tie
+    case playing
+}
+
+enum Turn: String {
+    case x
+    case o
+}
+
